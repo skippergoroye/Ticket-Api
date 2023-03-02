72d36c98603c0e70e121b0da69623b70f414622b
skippergoroye <oluwatosinadegoroye21@gmail.com>
2023-03-02 01:40:45 +0100

    create ticket done

--- controller/ticketController.js
+++ controller/ticketController.js
@@ -1,18 +1,59 @@
 const asyncHandler = require('express-async-handler')
 
-
 const User = require('../models/userModel')
 const Ticket = require('../models/ticketModel')
 
 
 
+// @desc   Get User tickets
+// @route  Get /api/users
+// @access Private
 const getTickets = asyncHandler( async (req, res)=> {
-    res.status(200).json({ message: 'getTickets'})
+    // Get User using the id in  the JWT
+    const user = await User.findById(req.user.id)
+
+    if(!user){
+        res.status(401)
+        throw new Error('User Not found')
+    }
+
+    const tickets = await Ticket.find({ user: req.user.id })
+
+    res.status(200).json(tickets)
 })
 
 
+
+
+
+// @desc   Create new tickets
+// @route  POST /api/users
+// @access Private
 const createTicket = asyncHandler(async(req, res) => {
-    res.status(200).json({ message: "createTicket"})
+    const { product, description } = req.body
+
+    if(!product || !description) {
+      res.status(400)
+      throw new Error("Please add a product and description")
+    }
+
+
+    // Get user using the id in the jwt
+    const user = await User.findById(req.user.id)
+
+    if(!user) {
+      req.status(401)
+      throw new Error('User not found')
+    }
+
+    const ticket = await Ticket.create({
+        product,
+        description,
+        user: req.user.id,
+        status: 'new'
+    })
+
+    res.status(201).json({ ticket })
 })
 
 
